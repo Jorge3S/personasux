c4dcfa50d9cc65445151ad97d54b1e1da7781652
Jorge3S <124838059+Jorge3S@users.noreply.github.com>
2023-03-09 15:52:02 -0600

    HelloWorld

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+x<template>
+  <HelloWorld msg=""/>
+  <h1 class="text-3xl font-bold underline">
+    Diseño de personas ux
+  </h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/App.vue",
+    "./src/components/HelloWorld.vue",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
