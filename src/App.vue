4f9acb04c597252ac6bdfb549331a09c14704f68
Jorge3S <124838059+Jorge3S@users.noreply.github.com>
2023-06-17 18:22:31 -0600

    personasuxd

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav class="flex items-center bg-blue-600 p-3 flex-wrap">
+    <a href="#" class="p-2 mr-4 inline-flex items-center">
+
+      <span class="text-xl text-black font-bold uppercase tracking-wide">Programación Web II</span>
+    </a>
+    <button
+      class="text-black inline-flex p-3 hover:bg-gray-700 rounded lg:hidden ml-auto hover:text-gray outline-none nav-toggler"
+      data-target="#navigation">
+      <i class="material-icons">
+        MENÚ
+      </i>
+    </button>
+    <div class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto" id="navigation">
+      <div
+        class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+        <a href="#"
+          class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+          <span>
+            <RouterLink to="/">Inicio</RouterLink>
+          </span>
+        </a>
+        <a href="#"
+          class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+          <span>
+            <RouterLink to="/about">Registrarse</RouterLink>
+          </span>
+        </a>
+        <a href="#"
+          class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+          <span>
+            <RouterLink to="/mostrar" class="py-5">Ver registros</RouterLink>
+          </span>
+        </a>
+
+      </div>
+    </div>
+  </nav>
+  <RouterView />
+</template>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="relative bg-[url(https://w.forfun.com/fetch/3f/3f3cffb82c2c1fdc785b9930f4e111b0.jpeg)] bg-cover bg-center bg-no-repeat">
+    <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"></div>
+
+    <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+      <div class="max-w-xl text-center sm:center">
+        <h1 class="text-3xl font-extrabold sm:text-5xl">
+          ¿Qué tipo de persona UXD eres? ¡Averigualo!
+          <strong class="block font-extrabold text-red-500">
+            
+          </strong>
+        </h1>
+        <div class="mt-8 flex flex-wrap gap-4 text-center">
+          <a href="#"
+            class="block w-full rounded bg-green-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-400 focus:outline-none focus:ring active:bg-green-600 sm:w-auto">
+            <RouterLink to="/about">Registraté</RouterLink>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
